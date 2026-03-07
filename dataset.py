d0b3d8f8d2065f5c91fd62679c882692cb0eec8d
adimac13 <s197932@student.pg.edu.pl>
2026-03-07 21:23:51 +0100

    Added dataset class

---
+++ dataset.py
@@ -0,0 +1,30 @@
+import string
+
+class Dataset:
+    def __init__(self, path):
+        # adding '\u2019' & '\u2018' to get rid of typographic encoding
+        punc = string.punctuation + '\u2019' + '\u2018'
+        translator = str.maketrans('','', punc)
+        with open(path, 'r', encoding='utf-8') as f:
+            file = f.read().lower()
+            clean_text = file.translate(translator).split()
+        self.text = clean_text
+        self.word2label = {}
+        self.label2word = {}
+        self.text2num = list()
+
+    def setup(self):
+        l = set()
+        for el in self.text:
+            if el not in l:
+                l.add(el)
+                self.word2label.update({el:len(l)-1})
+                self.label2word.update({len(l)-1:el})
+            self.text2num.append(self.word2label[el])
+
+        return self.text2num, self.word2label, self.label2word
+
+if __name__ == "__main__":
+    pass
+    # ds = Dataset('text.txt')
+    # ds.setup()
---
+++ main.py
@@ -0,0 +1,2 @@
+import numpy as np
+from dataset import Dataset
